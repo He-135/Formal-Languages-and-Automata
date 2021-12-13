b21b060465f211c64cfc54cadb398cff159628b9
He-135 <2069622218@qq.com>
2021-12-13 19:14:18 +0800

    修复了bug
    Signed-off-by: He-135 <2069622218@qq.com>

--- DFA-minimizeDFA/自动机作业/Main.cpp
+++ DFA-minimizeDFA/自动机作业/Main.cpp
@@ -37,37 +37,67 @@ int** DFA_Minimize(int** matrix, int end[], int count, int count_end, vector<int
 bool isEnd(int q, int end[], int count_end);
 int transition(int q, int ch, int** matrix, int count);//返回状态q接收ch后所到达的状态
 void tagRelevance(vector<List*>&v, int q1, int q2, int**tag);
+//void tagRelevance(vector<List*>&v, int q1, int q2, int tag[][9]);
+
 void save(vector<vector<int>>&result, int q1, int q2);
 bool inVec(vector<int>result, int temp);
 int main(void){
-	int count = 6;//状态个数
-
-	int** matrix = new int*[count];
-	for(int i = 0; i < count; i++){
+	//int count = 6;//状态个数
+	//int** matrix = new int*[count];
+	//for(int i = 0; i < count; i++){
+	//	matrix[i] = new int[count];
+	//	for(int j = 0; j < count; j++){
+	//		matrix[i][j] = -1;//初始化为-1，-1表示i和j之间无边
+	//	}
+	//}
+	//matrix[0][1] = 0;
+	//matrix[1][0] = 0;
+	//matrix[0][2] = 1;
+	//matrix[1][3] = 1;
+	//matrix[2][4] = 0;
+	//matrix[3][5] = 1;
+	//matrix[2][5] = 1;
+	//matrix[3][4] = 0;
+	//matrix[4][5] = 1;
+	//matrix[4][4] = 0;
+	//matrix[5][5] = 2;
+	//int end[] = { 2, 3, 4 };
+	int count = 10;//状态个数
+	int** matrix = new int* [count];
+	for (int i = 0; i < count; i++) {
 		matrix[i] = new int[count];
-		for(int j = 0; j < count; j++){
+		for (int j = 0; j < count; j++) {
 			matrix[i][j] = -1;//初始化为-1，-1表示i和j之间无边
 		}
 	}
 	matrix[0][1] = 0;
-	matrix[1][0] = 0;
-	matrix[0][2] = 1;
-	matrix[1][3] = 1;
-	matrix[2][4] = 0;
-	matrix[3][5] = 1;
-	matrix[2][5] = 1;
-	matrix[3][4] = 0;
-	matrix[4][5] = 1;
-	matrix[4][4] = 0;
-	matrix[5][5] = 2;
-	int end[] = { 2, 3, 4 };
+	matrix[0][5] = 1;
+	matrix[1][5] = 0;
+	matrix[1][2] = 1;
+	matrix[2][3] = 0;
+	matrix[2][6] = 1;
+	matrix[3][2] = 0;
+	matrix[3][4] = 1;
+	matrix[4][8] = 0;
+	matrix[4][1] = 1;
+	matrix[5][1] = 0;
+	matrix[5][6] = 1;
+	matrix[6][2] = 1;
+	matrix[6][7] = 0;
+	matrix[7][6] = 0;
+	matrix[7][8] = 1;
+	matrix[8][4] = 1;
+	matrix[8][5] = 0;
+	matrix[9][5] = 1;
+	matrix[9][7] = 0;
+	int end[] = { 4, 8 };
 	vector<int> minimizeEnd;
 	int countMinimize = 0;
 	int** minimizeDFA = DFA_Minimize(matrix, end, count, sizeof(end) / sizeof(int), minimizeEnd, countMinimize);
 	//输出
 	int temp;
 	cout << "\t0\t1" << endl;
-	cout << "#";	
+	cout << "#";
 	for(int i = 0; i < countMinimize; i++){
 		if (inVec(minimizeEnd, i)) { cout << "*"; }//为接收状态
 		cout << "q" << (i+1) << ":\t";
@@ -127,33 +157,49 @@ int main(void){
 
 int** DFA_Minimize(int** matrix, int end[], int count, int count_end, vector<int>& minimizeEnd, int& countMinimize){
 	//先删除不可达状态，某一列均为-1则为不可达状态
-	for(int i = 0; i < count; i++){//i表示第i列
+	for (int i = 1; i < count; i++) {//i表示第i列；从i=1开始是因为起始状态q0永远可达
 		bool reach = false;
-		for(int j = 0; j < count; j++){//j表示第j行
-			if(i != j && matrix[j][i] != -1){//说明第i个状态可达,继续判断第i+1列（第i+1个状态）
+		for (int j = 0; j < count; j++) {//j表示第j行
+			if (i != j && matrix[j][i] != -1) {//说明第i个状态可达,继续判断第i+1列（第i+1个状态）
 				reach = true;
 				break;
 			}
 		}
-		if(!reach){//如果该状态不可达，则可删除所有以状态i为起始的边
-			for(int k = 0; k < count; k++){
-				if(matrix[i][k] != -1){//存在以状态i为起始的边
+		if (!reach && i != 0) {//如果该状态不可达，则可删除所有以状态i为起始的边（注意：这里起始状态不能删除）
+			for (int k = 0; k < count; k++) {
+				if (matrix[i][k] != -1) {//存在以状态i为起始的边
 					matrix[i][k] = -1;
 				}
 			}
+			int temp = i;
+			//第i行之后的每一行需要往上移
+			for (; i + 1 < count; i++) {
+				for (int k = 0; k < count; k++) {
+					matrix[i][k] = matrix[i + 1][k];
+				}
+			}
+			//第i列右边的每一列需要往左移
+			for (i = temp; i + 1 < count; i++) {
+				for (int k = 0; k < count; k++) {
+					matrix[k][i] = matrix[k][i + 1];
+				}
+			}
+			count--;
+			i = 0;
 		}
 	}
+
 	int** tag = new int* [count];//用于标记两状态是否可区分
 	for (int i = 0; i < count; i++) {
 		tag[i] = new int[count];
 	}
-	
-	//0为可区分，1为不可区分，初始化均为0（0表示未标记）
-	for(int i = 1; i < count; i++){//每一列从q1开始，每一行从q0开始
-		for(int j = 0; j < i; j++){
-			tag[i][j] = 0;
-		}
-	}
+	//int tag[9][9];
+	////0为可区分，1为不可区分，初始化均为0（0表示未标记）
+	//for(int i = 1; i < count; i++){//每一列从q1开始，每一行从q0开始
+	//	for(int j = 0; j < i; j++){
+	//		tag[i][j] = 0;
+	//	}
+	//}
 	//与终止状态有关的状态对一定是不可区分的
 	for(int i = 0; i < count_end; i++){
 		//以下j表示行，k表示列
@@ -233,6 +279,13 @@ int** DFA_Minimize(int** matrix, int end[], int count, int count_end, vector<int
 			}
 		}
 	}
+	if (result.size() == 0) {//无可合并状态
+		cout << "无可合并状态" << endl;
+		countMinimize = count;
+		for (int i = 0; i < count_end; i++) { minimizeEnd.push_back(end[i]); }
+		return matrix;
+	}
+
 	for(int i = 0; i < count; i++){
 		if(!check[i]){
 			vector<int> temp;
@@ -240,47 +293,40 @@ int** DFA_Minimize(int** matrix, int end[], int count, int count_end, vector<int
 			result.push_back(temp);
 		}
 	}
-	if(result.size() == 0){//无可合并状态
-		countMinimize = count;
-		for (int i = 0; i < count_end; i++) { minimizeEnd.push_back(end[i]); }
-		return matrix;
+	//将不可区分的状态合并后生成最小化DFA
+	countMinimize = result.size();
+	for (int i = 0; i < count_end; i++) {
+		for (int j = 0; j < result.size(); j++) {
+			if (inVec(result[j], end[i]) && !inVec(minimizeEnd, j)) {
+				minimizeEnd.push_back(j);
+				break;
+			}
+		}
 	}
-	else{
-		//将不可区分的状态合并后生成最小化DFA
-		countMinimize = result.size();
-		for(int i = 0; i < count_end; i++){
-			for(int j = 0; j < result.size(); j++){
-				if(inVec(result[j], end[i]) && !inVec(minimizeEnd, j)){
-					minimizeEnd.push_back(j);
-					break;
-				}
-			}
+	int** matrix_new = new int* [result.size()];
+	for (int i = 0; i < result.size(); i++) {
+		matrix_new[i] = new int[result.size()];
+		for (int j = 0; j < result.size(); j++) {
+			matrix_new[i][j] = -1;//初始化为-1，-1表示i和j之间无边
 		}
-		int** matrix_new = new int* [result.size()];
-		for (int i = 0; i < result.size(); i++) {
-			matrix_new[i] = new int[result.size()];
-			for (int j = 0; j < result.size(); j++) {
-				matrix_new[i][j] = -1;//初始化为-1，-1表示i和j之间无边
-			}
-		}
-		for (int i = 0; i < result.size(); i++) {
-			int temp = transition(result[i][0], 0, matrix, count);
-			for (int j = 0; j < result.size(); j++) {
-				if (inVec(result[j], temp)) {
-					matrix_new[i][j] += 1;
-					break;
-				}
-			}
-			temp = transition(result[i][0], 1, matrix, count);
-			for (int j = 0; j < result.size(); j++) {
-				if (inVec(result[j], temp)) {
-					matrix_new[i][j] += 2;
-					break;
-				}
-			}
-		}
-		return matrix_new;
 	}
+	for (int i = 0; i < result.size(); i++) {
+		int temp = transition(result[i][0], 0, matrix, count);
+		for (int j = 0; j < result.size(); j++) {
+			if (inVec(result[j], temp)) {
+				matrix_new[i][j] += 1;
+				break;
+			}
+		}
+		temp = transition(result[i][0], 1, matrix, count);
+		for (int j = 0; j < result.size(); j++) {
+			if (inVec(result[j], temp)) {
+				matrix_new[i][j] += 2;
+				break;
+			}
+		}
+	}
+	return matrix_new;
 }
 
 bool isEnd(int q, int end[], int count_end){//使用二分法进行判断一个状态是否是接收状态
@@ -305,17 +351,19 @@ int transition(int q, int ch, int** matrix, int count){
 }
 
 void tagRelevance(vector<List*>&v, int q1, int q2, int**tag){
-	vector<List*>::iterator it = v.begin();
+//void tagRelevance(vector<List*>&v, int q1, int q2, int tag[][9]){
 	for(int i = 0; i < v.size(); i++){
 		if(v[i]->inList(q1, q2)){//状态q1和q2在状态关联表上
 			List* temp = v[i];
+			v.erase(v.begin() + i);
 			while(temp != NULL){
 				tag[temp->q1][temp->q2] = 1;
+				if(temp->q1 != q1 && temp->q2 != q2){
+					tagRelevance(v, temp->q1, temp->q2, tag);
+				}
 				temp = temp->next;
 			}
-			it = v.erase(it);
 		}
-		else { it++; }
 	}
 }
 
